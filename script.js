f4f44365bbbd0dffb89ee0017399124ce7f6ec9f
jhong <jhong@switchfly.com>
2016-04-05 22:14:13 -0700

    updated favicon, modal, styled story, day of, added rsvp

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 $("#party img").on("click", function() {
-  $(this).siblings(".bio").toggle();
+  var _elementClone = $(this).siblings(".bio").clone()
+  _elementClone.appendTo('.modal-body');
+  $(".modal-body .bio").toggle();
 });
 
-$("#party .close").on("click", function() {
-  $(this).parents(".bio").toggle();
-});
+$('#myModal').on('hidden.bs.modal', function (e) {
+  $(".modal-body")[0].innerHTML = ""
+})
 
 $(".carousel").slick({
     centerMode: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
 });
